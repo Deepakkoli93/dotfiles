fbb722c3c0257eab242224916025ba1087d7191b
Deepak Koli <Deepakkoli93@gmail.com>
2018-04-09 23:59:18 +0530

    Add timestamp on closing org-mode items

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -60,3 +60,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;; org-mode configuration
+
+;;show timestamp on closing items
+(setq org-log-done t)
